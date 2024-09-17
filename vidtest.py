9f9d4af6b5dc3d6fa11b32944bb30facac3194de
anShape <ihsan.nurkhotib-2020@ftmm.unair.ac.id>
2024-09-17 12:35:42 +0700

    the result already shown

--- vidtest.py
+++ vidtest.py
@@ -1,24 +1,25 @@
 # from ultralytics import YOLOv10
+import numpy as np
 from ultralytics import YOLO
 # model = YOLOv10.from_pretrained('jameslahm/yolov10n')
 # ncnn_model = YOLOv10('yolov10n_ncnn_model')
 # model = YOLO('yolov8n_ncnn_model')
 model = YOLO('yolov8n.pt')
 
-source = "rtsp://raspi.local:8554/cam"
+# source = "rtsp://raspi.local:8554/cam"
+# source = "camera.streams"
 
-results = model.predict(source, show=True, stream=True)
-# results = model.predict(source="0", show=True, stream=True)
+# results = model.predict(source, show=True, stream=True)
+results = model.predict(source="0", show=True, stream=True)
 # results = model(source="0", show=True, save_txt=True)
 # results = model.predict(source="0", stream=True)
 
 # for result in results:
 #      result.save_txt("output.txt")
 
+print(results[0].orig_img)
+
 for result in results:
-    boxes = result.boxes  # Boxes object for bounding box outputs
-    masks = result.masks  # Masks object for segmentation masks outputs
-    keypoints = result.keypoints  # Keypoints object for pose outputs
-    probs = result.probs  # Probs object for classification outputs
-    obb = result.obb  # Oriented boxes object for OBB outputs
-    # result.show()  # display to screen
+    boxes = result.boxes # Boxes object for bounding box outputs
+    print(result.orig_img) # original image
+    
